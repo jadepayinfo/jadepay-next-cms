8d83f2dcaaa55a0077be9533a4f6f70b52af04b1
warin yamyim <warin.y@inception.asia>
2025-10-27 15:56:49 +0700

    notification

---
+++ pages/api/notification/send_notification.ts
@@ -0,0 +1,25 @@
+import { Backend } from '@/lib/axios';
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+   //const { phoneNo, subject, description, image } = req.body;
+    //const payload = { phoneNo, subject, description, image   }
+    const accessToken = req.cookies['token']
+    const headers = { 'Authorization': `Bearer ${accessToken}` }
+    const response = await Backend.post(`/api/v1/gateway/sendNotification`, req.body, {headers});
+    res.json({ success: true, ...response.data.data  })
+
+  } catch (error: any) {
+    console.log('/api/v1/gateway/sendNotification', error)
+    res
+      .status(500)
+      .send({
+        success: false,
+        message: error.message ?? "Internal Server Error",
+      });
+  }
+}
